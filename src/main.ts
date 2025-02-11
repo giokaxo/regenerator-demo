bcb2a067217f138bd4943d3f37eab2e157292db8
Giorgi Kakhoshvili <giokaxo@gmail.com>
2025-02-11 19:10:22 +0100

    remove api key public usage

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,16 @@ import './style.css';
 import Client from '../sdk/client';
 import Collection from '../sdk/collection';
 
+let client: Client;
+let storedApiKey = '';
 const outputEl = document.getElementById('output') as HTMLPreElement;
-const client = new Client({
-  apiKey: 'sk_sandbox_f6SPfpm_EbhKtbqbgzjG5vF8YuT7DgvXRZbUjVS',
-});
+const apiKeyInput = document.getElementById('api-key') as HTMLInputElement;
+
+function initClient(apiKey: string) {
+  client = new Client({
+    apiKey,
+  });
+}
 
 function showOutput(data: Collection | string) {
   outputEl.textContent = JSON.stringify(data, null, 2);
@@ -14,22 +20,31 @@ function showOutput(data: Collection | string) {
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 document.addEventListener('click', async (event) => {
   const target = event.target as HTMLElement;
-  if (target.classList.contains('fetch-button')) {
-    const action = target.dataset.fetch;
-    showOutput('Loading...');
-    await sleep(1000);
-    switch (action) {
-      case 'api-keys':
-        fetchApiKeys();
-        break;
-      case 'customers':
-        fetchCustomers();
-        break;
-      case 'value-lists':
-        fetchValueLists();
-        break;
-      case 'applications':
-    }
+  if (!target.classList.contains('fetch-button')) {
+    return;
+  }
+  if (apiKeyInput.value === '') {
+    alert('Please enter an API key');
+    return;
+  }
+  if (!client || storedApiKey !== apiKeyInput.value) {
+    initClient(apiKeyInput.value);
+    storedApiKey = apiKeyInput.value;
+  }
+  const action = target.dataset.fetch;
+  showOutput('Loading...');
+  await sleep(1000);
+  switch (action) {
+    case 'api-keys':
+      fetchApiKeys();
+      break;
+    case 'customers':
+      fetchCustomers();
+      break;
+    case 'value-lists':
+      fetchValueLists();
+      break;
+    case 'applications':
   }
 });
 
