b477184ac616e951dbd240dbca71f222ba89f854
Giorgi Kakhoshvili <giokaxo@gmail.com>
2025-02-11 19:42:31 +0100

    add log

--- src/main.ts
+++ src/main.ts
@@ -50,18 +50,51 @@ document.addEventListener('click', async (event) => {
 
 async function fetchCustomers() {
   const customersCollection = await client.customers.getAll();
+  console.log('%cCollection', 'font-size: 16px;font-weight:bold;color:red');
   console.log(customersCollection);
+  for (const item of customersCollection) {
+    console.log('%cItem', 'font-size: 16px;font-weight:bold;color:red');
+    console.log({
+      id: item.id,
+      firstName: item.firstName,
+      lastName: item.lastName,
+      email: item.email,
+    });
+  }
   showOutput(customersCollection);
 }
 
 async function fetchApiKeys() {
   const apiKeysCollection = await client.apiKeys.getAll();
+  console.log('%cCollection', 'font-size: 16px;font-weight:bold;color:red');
   console.log(apiKeysCollection);
+  for (const item of apiKeysCollection) {
+    console.log('%cItem', 'font-size: 16px;font-weight:bold;color:red');
+    console.log({
+      apiUser: item.apiUser,
+      allowedIps: item.allowedIps,
+      createdAt: item.createdTime,
+      updatedAt: item.updatedTime,
+      secretKey: item.secretKey,
+    });
+  }
   showOutput(apiKeysCollection);
 }
 
 async function fetchValueLists() {
   const valueListsCollection = await client.lists.getAll();
+  console.log('%cCollection', 'font-size: 16px;font-weight:bold;color:red');
   console.log(valueListsCollection);
+  for (const item of valueListsCollection) {
+    console.log('%cItem', 'font-size: 16px;font-weight:bold;color:red');
+    console.log({
+      id: item.id,
+      version: item.version,
+      description: item.description,
+      values: item.values,
+      createdAt: item.createdTime,
+      updatedAt: item.updatedTime,
+    });
+  }
   showOutput(valueListsCollection);
 }
